3da2831e1f62a68febd6d0e4081d47545597ba31
InfamousEli02 <ejreed328@gmail.com>
2025-04-02 21:03:40 -0400

    event handlers are another way for the javascript to react to a user's action, but  done directly on the HTML

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+//When DOM is fully loaded, this will be fired
 document.addEventListener("DOMContentLoaded", function() {
     let onLoad = document.getElementById("onLoad");
 
     onLoad.innerHTML = "<h1>This is some loaded content</h1>"
-})
+
 
 //Get the element that will show us our key code
 const directionEl = document.getElementById("direction");
@@ -20,4 +21,24 @@ let buttonClick = document.getElementById("clickMe");
 
 buttonClick.addEventListener("click", function() {
     buttonClick.innerText = `You clicked me!`;
+    
+    
+
 })
+
+    
+//event handlers are done directly on the HTML, event listeners are done in js
+
+// function greet(){
+//     console.log('Hello!')
+// }
+})
+let changeButton = document.getElementById('randomColorBtn');
+let newColor = {1:'pink', 2:'orange', 3:'red', 4:'yellow', 5:'pink', 6:'orange', 
+    7:'red', 8:'blue', 9:'green', 10:'teal'};
+    let changeColor = function(){
+        let currentColor = Math.ceil(Math.random() * 10);
+        let screenBody = document.body.style;
+        console.log(newColor[currentColor]);
+        screenBody.background = newColor[currentColor];
+    }
